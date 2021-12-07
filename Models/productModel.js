88f97e8ffac72864afdad46ecc77183d7b300c76
kokohans <hw.wiranata@gmail.com>
2021-12-07 15:52:18 +0700

    JC1-17_fix add stock information

--- Models/productModel.js
+++ Models/productModel.js
@@ -39,9 +39,11 @@ class Product {
     let query = `SELECT idproducts, name, price, description, size 
             FROM products 
             JOIN package ON products.idpackage = package.idpackage
-            WHERE idproducts=${escaped_id}; SELECT url FROM picture_by_product WHERE idproduct=${escaped_id}`;
+            WHERE idproducts=${escaped_id}; 
+            SELECT url FROM picture_by_product WHERE idproduct=${escaped_id}; 
+            SELECT SUM(qty) as 'all_stock' FROM stock_product_by_warehouse WHERE idproducts=${escaped_id}`;
 
-    db.query(query, [1, 2], (err, results) => {
+    db.query(query, [1, 2, 3], (err, results) => {
       if (err) callback({ err: true, message: "query error" });
       let product = {
         idproducts: results[0][0].idproducts,
@@ -49,6 +51,7 @@ class Product {
         price: results[0][0].price,
         description: results[0][0].description,
         size: results[0][0].size,
+        stock: results[2][0].all_stock,
         urls: results[1].map((data) => data.url),
       };
 
